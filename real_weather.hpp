18b68612edae664b723d3cce0db88e43b6fe54f5
Nicolas BOITEUX <nicolas_boiteux@yahoo.fr>
2013-11-25 07:49:27 +0100

    add game parameters

---
+++ real_weather.hpp
@@ -0,0 +1,20 @@
+	class PARAM_TimeOfDay {
+		title = "    Time of Day";
+		values[] = {1,2,3,4};
+		texts[] = {"04:00", "12:00", "17:00", "22:00"};
+		default = 2;
+	};
+
+	class PARAM_fastime {
+		title = "    Fast time";
+		values[] = {0,1};
+		texts[] = {"No", "Yes"};
+		default = 1;
+	};
+
+	class PARAM_dynamicweather {
+		title = "    Dynamic Weather";
+		values[] = {0,1};
+		texts[] = {"No", "Yes"};
+		default = 1;
+	};
